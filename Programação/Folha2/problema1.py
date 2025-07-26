80693ee7b5f4f338ec4936ba5e5e0c353c90a357
Gonçalo Martins <gonmartins7@gmail.com>
2025-07-26 13:51:47 +0100

    Delete Programação/Folha2 directory

--- Programação/Folha2/problema1.py
+++
@@ -1,5 +0,0 @@
-numero = int(input("Escreva um número interio: "))
-if numero%2==0 :
-    print("O número escrito é par.")
-else :
-    print("O número escrito é ímpar.")
--- Programação/Folha2/problema3.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Escreva um número: "))
-y = int(input("Escreva ainda outro número: "))
-
-if (x > y):
-    print("O número", x, "é o maior dos dois números que inseriu.")
-elif (y > x):
-    print("O número", y, "é o maior dos dois números que inseriu.")
-else:
-    print("Os números são iguais.")
